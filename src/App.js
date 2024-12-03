34d423570c12edc45fbe4b623cd3a348d153e3ae
LGH <ignore0311@naver.com>
2024-12-03 13:54:07 +0900

    study

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
+import Calc1 from './comp/clac/study01'
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <About />
         <Routes>
           <Route path={"/"} element={<Home />} />
           <Route path={"/about"} element={<About />} />
+          <Route path={"/cal1"} element={<Calc1 />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -16,9 +20,8 @@ function App() {
 }
 
 function About() {
-  return(
-    <div>
-      <h1>About</h1>
+  return (
+    <div style={{border: '2px blue solid'}}>
       <Link to="/">Home으로 이동</Link>
     </div>
   )
@@ -28,7 +31,9 @@ function Home() {
   return(
     <div>
       <h1>Start Home</h1>
-      <Link to="/about">About으로 이동</Link>
+      <Link to="/about">About으로 이동</Link><br/>
+      <Link to="/cal1">Cal1로 이동하기</Link>
+
     </div>
   )
 }
